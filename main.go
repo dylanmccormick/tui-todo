322763919f2c294e9f754f6f5d9d9814554b4161
Dylan McCormick <dylanm.bsa@gmail.com>
2025-09-24 07:08:07 -0400

    first whispers of a project

--- main.go
+++ main.go
@@ -2,114 +2,112 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"os"
-	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 const url = "https://charm.sh"
 
-type model struct {
-	choices  []string
-	cursor   int
-	selected map[int]struct{}
-	status   int
-	err      error
+type task struct {
+	name        string
+	assignee    string
+	description string
+	status      status
+	tags        []string
 }
 
+type taskList struct {
+	tasks    []task
+	cursor   int
+	selected map[int]struct{}
+}
+
+type status int
+
+const (
+	NOT_STARTED status = iota
+	DOING
+	DONE
+)
+
 func main() {
-	p := tea.NewProgram(initialModel())
+	p := tea.NewProgram(baseList())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there has been an error: %v", err)
 		os.Exit(1)
 	}
 }
 
-func initialModel() model {
-	return model{
-		choices:  []string{"Buy carrots", "Buy Celery", "make soup"},
-		selected: make(map[int]struct{}),
+func baseList() taskList {
+	tasks := []task{
+		{
+			name:        "take out garbage",
+			description: "put the garbage in the government garbage can",
+			assignee:    "Dylan",
+			status:      NOT_STARTED,
+			tags:        []string{"chores"},
+		},
+		{
+			name:        "do the dishes",
+			description: "clean all of the dishes in the sink",
+			assignee:    "Dylan",
+			status:      DONE,
+			tags:        []string{"chores"},
+		},
 	}
+	return taskList{tasks: tasks, selected: make(map[int]struct{})}
 }
 
-func (m model) Init() tea.Cmd {
-	return checkServer
+func (tl taskList) Init() tea.Cmd {
+	return nil
 }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (tl taskList) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	case statusMsg:
-		m.status = int(msg)
-		return m, nil
-
-	case errMsg:
-		m.err = msg
-		return m, tea.Quit
-
-
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q": // WHO KNEW YOU COULD PUT MULTIPLE CHOICES IN A CASE STATEMENT
-			return m, tea.Quit
+			return tl, tea.Quit
 		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
+			if tl.cursor > 0 {
+				tl.cursor--
 			}
 		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
+			if tl.cursor < len(tl.tasks)-1 {
+				tl.cursor++
 			}
 		case "enter", " ":
-			_, ok := m.selected[m.cursor]
+			_, ok := tl.selected[tl.cursor]
 			if ok {
-				delete(m.selected, m.cursor)
+				delete(tl.selected, tl.cursor)
 			} else {
-				m.selected[m.cursor] = struct{}{}
+				tl.selected[tl.cursor] = struct{}{}
 			}
 
 		}
 	}
-	return m, nil
+	return tl, nil
 }
 
-func (m model) View() string {
-	s := "What should we buy at the market?\n\n"
+func (tl taskList) View() string {
+	s := "My List of Tasks:\n\n"
 
-	for i, choice := range m.choices {
+	for i, task := range tl.tasks {
 		cursor := " "
-		if m.cursor == i {
+		if tl.cursor == i {
 			cursor = ">"
 		}
 
 		checked := " "
-		if _, ok := m.selected[i]; ok {
+		if _, ok := tl.selected[i]; ok {
 			checked = "x"
 		}
 
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, task.name)
 	}
 
-	s += fmt.Sprintf("%d %s!", m.status, http.StatusText(m.status))
 	s += "\nPress q to quit.\n"
 
 	return s
 }
-
-func checkServer() tea.Msg {
-	c := &http.Client{Timeout: 10 * time.Second}
-	res, err := c.Get(url)
-	if err != nil {
-		return errMsg{err}
-	}
-
-	return statusMsg(res.StatusCode)
-}
-
-type (
-	statusMsg int
-	errMsg    struct{ err error }
-)
-
-func (e errMsg) Error() string { return e.err.Error() }
